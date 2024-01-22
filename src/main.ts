92735cef35fda4eea93b2c90369dd8dccae13d41
123yael <119128695+123yael@users.noreply.github.com>
2024-01-22 23:13:21 +0200

    add cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  });
   await app.listen(7085);
 }
 bootstrap();
