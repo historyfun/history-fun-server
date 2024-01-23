490996949e98fb8676785bf1f01fb7ee8d0e9af8
123yael <119128695+123yael@users.noreply.github.com>
2024-01-23 16:15:05 +0200

    add cors

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: false,
-  });
+  app.use(cors({ origin: 'https://history-fun.vercel.app' }));
+  // app.enableCors({
+  //   origin: '*',
+  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  // });
   await app.listen(7085);
 }
 bootstrap();
