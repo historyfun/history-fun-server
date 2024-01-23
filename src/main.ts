1f7b29ff15b8723a6fe5653be21c6e0428d36045
123yael <119128695+123yael@users.noreply.github.com>
2024-01-23 17:38:20 +0200

    change vercel.jsom

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-// import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // app.use(cors({ origin: 'https://history-fun.vercel.app' }));
-  // app.use(cors());
-  app.enableCors();
-  // app.enableCors({
-  //   origin: '*',
-  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  // });
+  app.enableCors({
+    origin: ['https://history-fun.vercel.app', 'http://localhost:3000'],
+    methods: 'GET, HEAD, PUT, POST, DELETE, OPTIONS, PATCH',
+    credentials: true,
+    allowedHeaders:
+      'Origin, X-Requested-With, Content-Type, Accept, Authentication, Access-control-allow-credentials, Access-control-allow-headers, Access-control-allow-methods, Access-control-allow-origin, User-Agent, Referer, Accept-Encoding, Accept-Language, Access-Control-Request-Headers, Cache-Control, Pragma',
+  });
   await app.listen(7085);
 }
 bootstrap();
